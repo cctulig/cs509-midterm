387352cbcd2bc669aa164e04e1d64dba171a8718
cctulig <cctulig@gmail.com>
2024-03-03 12:18:59 -0500

    Login with backend

--- atm/atm/Program.cs
+++ atm/atm/Program.cs
@@ -9,12 +9,17 @@ public enum LoginState
     SIGNED_IN
 }
 
+public record UserLogin(int Id, string login, int pin, bool adminAccount);
+
 class Program
 {
     static void Main(string[] args)
     {
         LoginState loginState = LoginState.SIGNED_OUT;
         UserPage? userPage = null;
+        
+        string connStr = "server=localhost;user=root;database=cs509midterm;port=3306;password=a";
+        using var connection = new MySqlConnection(connStr);
 
         Console.WriteLine("ATM Application");
         Console.WriteLine("------------------------");
@@ -26,20 +31,41 @@ class Program
                     Console.WriteLine("Sign in!");
 
                     Console.WriteLine("Enter login:");
-                    string username = Console.ReadLine();
+                    string login = Console.ReadLine();
 
                     Console.WriteLine("Enter Pin code:");
-                    string password = Console.ReadLine();
+                    string pinStr = Console.ReadLine();
+                    
+                    if (!Int32.TryParse(pinStr, out int pin))
+                    {
+                        Console.WriteLine("Pin must be a number");
+                        continue;
+                    }
+                    
+                    var getUserParameters = new DynamicParameters();
+                    getUserParameters.Add("login", login, DbType.String, ParameterDirection.Input);
+                    getUserParameters.Add("pin", pin, DbType.Int32, ParameterDirection.Input);
 
-                    if (true)
+                    try
                     {
-                        userPage = new AdminPage(new Admin());
+                        UserLogin userLogin = connection.QueryFirst<UserLogin>(
+                            "SELECT * FROM userlogin WHERE login=@login AND pin=@pin;", getUserParameters);
+                        
+                        if (userLogin.adminAccount)
+                        {
+                            userPage = new AdminPage(new Admin());
+                        }
+                        else
+                        {
+                            userPage = new CustomerPage(new Customer());
+                        }
+                        loginState = LoginState.SIGNED_IN;
                     }
-                    else
+                    catch (Exception e)
                     {
-                        userPage = new CustomerPage(new Customer());
+                        Console.WriteLine("User not found");
                     }
-                    loginState = LoginState.SIGNED_IN;
+                    
                     break;
                 case LoginState.SIGNED_IN:
                     if (userPage != null)
